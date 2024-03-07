e30697534c6c99c5d25d0f016bc238e3c0961907
samrat53 <samrat05hccs@gmail.com>
2024-03-08 01:48:45 +0530

    functionality established

--- frontend/chatbot-frontend/src/App.jsx
+++ frontend/chatbot-frontend/src/App.jsx
@@ -5,67 +5,48 @@ function App() {
   const [replies, setReplies] = useState([]);
   const [getResponse, setGetResponse] = useState(false);
   const [userInput, setUserInput] = useState("");
+  const [history, setHistory] = useState([]);
 
-  const sendRequest = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userInput }), // Send userInput in the request body
-      });
-
-      const data = await response.json();
-      setReplies(prevReplies => [...prevReplies, data.response]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  useEffect(() => {
+    fetch(`http://localhost:3000/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    }).then(async (res) => {
+      const json = await res.json();
+      setReplies((prevReplies) => [...prevReplies, json.response]);
+      setHistory((prevHistory) => [...prevHistory, userInput]);
+      setUserInput("");
+    });
+  }, [getResponse]);
 
   return (
     <>
       <input
         placeholder="What you want to know about Samrat?"
         value={userInput}
+        name="userInput"
         onChange={(e) => setUserInput(e.target.value)}
       />
-      <button onClick={sendRequest}>Send</button>
+      <button onClick={() => setGetResponse(!getResponse)}>Send</button>
       <div>
         {replies.map((item, index) => (
           <RenderItems key={index} message={item} />
         ))}
       </div>
+      <h1>History:</h1>
+      <div>
+        {history.map((item, index) => (
+          <RenderItems key={index} message={item} />
+        ))}
+      </div>
     </>
   );
-
-  // useEffect(() => {
-  //   fetch(`http://localhost:3000/`).then(async (res) => {
-  //     const json = await res.json();
-  //     setReplies(prevReplies => [...prevReplies, json.response]);
-  //   });
-  // }, [getResponse]);
-  
-
-  // return (
-  //   <>
-  //     <input
-  //       placeholder="What you want to know about Samrat?"
-  //       value={userInput}
-  //       name="userInput"
-  //       onChange={(e) => setUserInput(e.target.value)}
-  //     />
-  //     <button onClick={() => setGetResponse(!getResponse)}>Send</button>
-  //     <div>
-  //       {replies.map((item, index) => (
-  //         <RenderItems key={index} message={item} />
-  //       ))}
-  //     </div>
-  //   </>
-  // );
 }
 
-const RenderItems = memo(({ message }) => {
+const RenderItems = memo(({ message,index }) => {
   return <div>{message}</div>;
 });
 
